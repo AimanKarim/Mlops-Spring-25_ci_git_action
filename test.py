ce9433c441120c6ce530e7171aa6dd58c025deae
Aiman <i210664@nu.edu.pk>
2025-01-28 11:08:12 +0500

    first commit

--- test.py
+++ test.py
@@ -5,3 +5,11 @@ def test_add():
 
 def test_subtract():    
     assert subtract(1,2) == -1
+
+def test_multiply():
+    assert multiply(1,2) == 2
+
+def test_divide(): 
+    assert divide(1,2) == 0.5
+    with pytest.raises(ValueError):
+        divide(1,0)
